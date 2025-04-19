a62690a785a991207ff010f7c7a1f20ddd50a013
Ayush Adhikari <adhikariayush19@gmail.com>
2025-04-19 08:46:30 +0545

    modified into class component

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,15 +1,10 @@
 from django.urls import path
-from django.shortcuts import redirect
 from . import views
 
-def redirect_to_todos(request):
-    return redirect('create-todo-form')
-
 urlpatterns = [
-    path('', redirect_to_todos, name='home'),
-    path('create/', views.createTodo, name='create-todo'),
-    path('list/', views.getTodos, name='get-todos'), 
-    path('update/<int:pk>/', views.updateTodo, name='update-todo'),
-    path('delete/<int:pk>/', views.deleteTodo, name='delete-todo'),
-    path('create-form/', views.createTodoForm, name='create-todo-form'),
+    path('create/', views.TodoAPIView.as_view(), name='create-todo'),
+    path('list/', views.TodoListView.as_view(), name='get-todos'),
+    path('update/<int:pk>/', views.TodoUpdateView.as_view(), name='update-todo'),
+    path('delete/<int:pk>/', views.TodoDeleteView.as_view(), name='delete-todo'),
+    path('create-form/', views.TodoFormView.as_view(), name='create-todo-form'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,18 +1,63 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-import json
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.generic.edit import FormView  # Add this import
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Todo
 from .forms import TodoForm
-from django.shortcuts import render, redirect
-from .forms import TodoForm
 
-@csrf_exempt
-def createTodo(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        todo = Todo.objects.create(**data)
-        
+@method_decorator(csrf_exempt, name='dispatch')
+class TodoAPIView(APIView):
+    def post(self, request):
+        todo = Todo.objects.create(**request.data)
+        return Response({
+            'message': 'Todo created successfully',
+            'todo': {
+                'id': todo.id,
+                'title': todo.title,
+                'description': todo.description
+            }
+        }, status=201)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class TodoListView(APIView):  # Change to APIView
+    def get(self, request):
+        todos = Todo.objects.filter(is_deleted=False)
+        todo_list = [{
+            'id': todo.id,
+            'title': todo.title,
+            'description': todo.description,
+            'is_completed': todo.is_completed
+        } for todo in todos]
+        return Response(todo_list) 
+
+@method_decorator(csrf_exempt, name='dispatch')
+class TodoUpdateView(APIView):
+    def put(self, request, pk):
+        todo = Todo.objects.get(pk=pk)
+        todo.title = request.data.get('title', todo.title)
+        todo.description = request.data.get('description', todo.description)
+        todo.is_completed = request.data.get('is_completed', todo.is_completed)
+        todo.save()
+        return Response({'message': 'Todo updated successfully'})
+
+@method_decorator(csrf_exempt, name='dispatch')
+class TodoDeleteView(View):
+    def delete(self, request, pk):
+        todo = Todo.objects.get(pk=pk)
+        todo.is_deleted = True
+        todo.save()
+        return JsonResponse({'message': 'Todo deleted successfully'})
+
+class TodoFormView(FormView):
+    template_name = 'todo_form.html'
+    form_class = TodoForm
+
+    def form_valid(self, form):
+        todo = form.save()
         return JsonResponse({
             'message': 'Todo created successfully',
             'todo': {
@@ -21,57 +66,6 @@ def createTodo(request):
                 'description': todo.description
             }
         }, status=201)
-    
-    return JsonResponse({'message': 'Method not allowed'}, status=405)
 
-
-@csrf_exempt
-def getTodos(request):
-    if request.method == 'GET':
-        todos = Todo.objects.filter(is_deleted=False)
-        todo_list = [{
-            'id': todo.id,
-            'title': todo.title,
-            'description': todo.description
-        } for todo in todos]
-        
-        return JsonResponse({'todos': todo_list}, safe=False)
-
-@csrf_exempt 
-def updateTodo(request, pk): 
-    if request.method == 'PUT':
-        data = json.loads(request.body)
-        todo = Todo.objects.get(pk=pk)
-        todo.title = data.get('title', todo.title)
-        todo.description = data.get('description', todo.description)
-        todo.is_completed = data.get('is_completed', todo.is_completed)
-        todo.save()
-
-        return JsonResponse({'message': 'Todo updated successfully'})
-    return JsonResponse({'message': 'Method not allowed'}, status=405)
-
-@csrf_exempt 
-def deleteTodo(request, pk):
-    if request.method == 'DELETE':
-        todo = Todo.objects.get(pk=pk)
-        todo.is_deleted = True
-        todo.save()
-        return JsonResponse({'message': 'Todo deleted successfully'})
-    return JsonResponse({'message': 'Method not allowed'}, status=405)
-    
-def createTodoForm(request):
-    if request.method == 'POST':
-        form = TodoForm(request.POST)
-        if form.is_valid():
-            todo = form.save()
-            return JsonResponse({
-                'message': 'Todo created successfully',
-                'todo': {
-                    'id': todo.id,
-                    'title': todo.title,
-                    'description': todo.description
-                }
-            }, status=201)
-    else:
-        form = TodoForm()
-    return render(request, 'todo_form.html', {'form': form})
+    def form_invalid(self, form):
+        return render(self.request, self.template_name, {'form': form})
